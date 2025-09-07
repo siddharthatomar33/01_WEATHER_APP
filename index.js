6c3ff3fc9126a0962b41d6318502275a408048ea
siddharthatomar33 <siddharthatomar33@gmail.com>
2025-09-07 12:57:09 +0530

    WEATHERAPP

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ function renderWeatherInfo(data){
 
 async function fetchWeatherDetails(){
     try{
-    // let cityname = "goa";
+    let cityname = "goa";
 
-    const response = await fetch('https://api.openweathermap.org/data/2.5/weather?q=goa&appid=225e6a742f20dbc82ecfbb710c306fbe');
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=${APIkey}&units=metric`);
     const data = await response.json();
 
     console.log(" weather data:-> ",data);
@@ -31,8 +31,8 @@ async function getCustomWeatherDetails() {
     try{
     let latitude=15.6333;
     let longitude=18.3333;
-    
-    let result=await fetch('https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon=({longitude}&appid=225e6a742f20dbc82ecfbb710c306fbe');
+    //we use backtick `` in api url
+    let result=await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=225e6a742f20dbc82ecfbb710c306fbe`);
     let data= await result.json();
     console.log(data);
     }
