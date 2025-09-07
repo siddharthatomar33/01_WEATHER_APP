2e0ffb11102666f78e39d656db3e02c0b47032bf
siddharthatomar33 <siddharthatomar33@gmail.com>
2025-09-07 08:02:42 +0530

    WEATHERAPP

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 console.log('hello jee tomar')
+
+const API_KEY="225e6a742f20dbc82ecfbb710c306fbe";
+
+async function showWeather(){
+    let city="goa";
+
+    const response=await fetch('https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}');
+
+    const data= await response.json();
+
+    console.log("weather data:->"+data);
+}
