3fca2e6c4b542baef89b224cefa8fa02fa1ba7eb
siddharthatomar33 <siddharthatomar33@gmail.com>
2025-09-07 12:48:37 +0530

    WEATHERAPP

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ console.log('hello jee tomar')
 
 // const APIkey="225e6a742f20dbc82ecfbb710c306fbe";
 
+function renderWeatherInfo(data){
+    let newPara=document.createElement('p');
+    newPara.textContent='${data?.main?.temp.toFixed(2)} C' 
+    
+    document.body.appendChild(newPara);
+}
+
 async function fetchWeatherDetails(){
     try{
     // let cityname = "goa";
@@ -11,12 +18,7 @@ async function fetchWeatherDetails(){
 
     console.log(" weather data:-> ",data);
 
-    // let newPara=document.createElement('p');
-    // newPara.textContent='${data?.main?.temp.toFixed(2)} C' 
-    
-    // document.body.appendChild(newPara);
-
-    renderWeatherinfo(data);
+    renderWeatherInfo(data);
     }
     catch(err){
         //handle the error
@@ -24,17 +26,17 @@ async function fetchWeatherDetails(){
     }
 }
 
+
 async function getCustomWeatherDetails() {
     try{
-
-    }
-    catch(err){
-        
-    }
     let latitude=15.6333;
     let longitude=18.3333;
     
-    let result=await fetch('https://api.openweathermap.org/data/2.5/weather?lat=15.6333&lon=18.3333&appid=225e6a742f20dbc82ecfbb710c306fbe');
+    let result=await fetch('https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon=({longitude}&appid=225e6a742f20dbc82ecfbb710c306fbe');
     let data= await result.json();
     console.log(data);
+    }
+    catch(err){
+        console.log("errrrror is:-> ",err);
+    }
 }
