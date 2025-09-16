6bca80551842c7b84bb024761bec2556bcec95c2
siddharthatomar33 <siddharthatomar33@gmail.com>
2025-09-16 12:27:10 +0530

    weatheraap

--- index.js
+++ index.js
@@ -106,7 +106,7 @@ function renderWeatherInfo(weatherInfo){
     desc.innerText=weatherInfo?.weather?.[0]?.description;
     weatherIcon.src=`http://openweathermap.org/img/w/${weatherInfo?.weather?.[0]?.icon}.png`;
     temp.innerText=`${weatherInfo?.main?.temp} °C`;
-    windSpeed.innerText=`${weatherInfo?.wind?.speed} `;
+    windSpeed.innerText=`${weatherInfo?.wind?.speed} km/h`;
     humidity.innerText=`${weatherInfo?.main?.humidity} %`;
     cloudiness.innerText=weatherInfo?.clouds?.all?? "N/A";
 
